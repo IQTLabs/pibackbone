7eda5507e0096be00893e0ff5622ab14a8073138
cglewis <clewis@iqt.org>
2022-03-11 10:28:53 -0800

    add initial questions

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ pip3 -V > /dev/null || echo "First install pip3 then try again."
 git --version > /dev/null || echo "First install git then try again."
 uname -m > /dev/null # TODO check for arm, and version
 
+# install docker if it's not already
+docker version || echo "Installing Docker..." && curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+
 # pull down pibackbone repo and install
 git clone https://github.com/iqtlabs/pibackbone 
 cd pibackbone && pip3 install . cd ..
