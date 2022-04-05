0924c6284bab7100bdab411e92fa25c2cdd07369
cglewis <clewis@iqt.org>
2022-04-05 13:43:45 -0700

    cleanup

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -x
 cwd=$PWD
 
 # install basics
-sudo apt-get update && sudo apt-get install -yq build-essential git python3-pip python3-smbus python3-urwid screen tmux
+sudo apt-get update && sudo apt-get install -yq build-essential ca-certificates git python3-certifi python3-pip python3-smbus python3-urwid screen tmux
 
 # check for dependencies
 python3 -V > /dev/null || echo "First install python3 then try again."
@@ -13,13 +13,16 @@ pip3 -V > /dev/null || echo "First install pip3 then try again."
 git --version > /dev/null || echo "First install git then try again."
 uname -m > /dev/null # TODO check for arm, and version
 
+# update pip
+python3 -m pip install --upgrade pip
+
 docker version > /dev/null || (echo "Installing Docker..." && curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && sudo usermod -aG docker "$(whoami)")
 
 # pull down pibackbone repo and install
 cd /opt
 sudo git clone https://github.com/iqtlabs/pibackbone 
 cd pibackbone
-pip3 install .
+sudo pip3 install .
 sudo cp scripts/cron.d/pibackbone /etc/cron.d/pibackbone
 
 # disable unneeded services
@@ -36,6 +39,7 @@ sudo systemctl disable apt-daily-upgrade.timer
 # requires a reboot to take effect
 sudo dphys-swapfile swapoff
 sudo sed -i '/CONF_SWAPSIZE/c\CONF_SWAPSIZE=8192' /etc/dphys-swapfile
+sudo sed -i '/CONF_MAXSWAP/c\CONF_MAXSWAP=' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
 
