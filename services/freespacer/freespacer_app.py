762824048bf74fb2eabbabc65f9a538bd3d49eb9
Josh Bailey <josh@vandervecken.com>
2022-09-16 00:10:47 +0000

    refresh disk usage percentage always.

--- services/freespacer/freespacer_app.py
+++ services/freespacer/freespacer_app.py
@@ -9,23 +9,21 @@ import time
 
 
 def make_free_space(path, min_used_pct):
-    usage = shutil.disk_usage(path)
 
-    def enough_free(used):
-        used_pct = used / usage.total * 100
+    def enough_free():
+        usage = shutil.disk_usage(path)
+        used_pct = usage.used / usage.total * 100
         logging.info("used space now %.f%%", used_pct)
         return (used_pct < min_used_pct)
 
     removed = []
-    if not enough_free(usage.used):
+    if not enough_free():
         files = sorted([(f, os.lstat(f)) for f in glob.glob(os.path.join(path, '*'), recursive=True) if os.path.isfile(f)], key=lambda x: x[1].st_ctime)
-        used_space = usage.used
         for file_name, file_stat in files:
             os.remove(file_name)
             removed.append(file_name)
             logging.info("removed %s, size %u", file_name, file_stat.st_size)
-            used_space -= file_stat.st_size
-            if enough_free(used_space):
+            if enough_free():
                 break
     return removed
 
